88f385c3728bfbdc874ee9260748ea3cc16c72b8
Muhammad Tsany Qudsi <tsanyqudsi@gmail.com>
2019-06-18 07:40:20 +0700

    add upload csv

---
+++ resources/views/vendor/voyager/orders/browse.blade.php
@@ -0,0 +1,13 @@
+@extends('voyager::bread.browse')
+
+@section('page_header')
+    @parent
+    <div class='container-fluid'>
+        <form>
+            <div class='form-group'>
+                    <input class='csv' type='file' name='filename'/>
+                    <button class='btn btn-info'><i class='voyager-upload'></i> Upload CSV</button>
+            </div>
+        </form>
+    </div>
+@stop
